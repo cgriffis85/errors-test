4281a7c137d1b4c2c91a05c0e75e06ce8a1a6b25
cgriffis85 <81969903+cgriffis85@users.noreply.github.com>
2021-05-09 22:19:58 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int yearsTillDoomsday = 15645454;
+        //We are defining our variable (integer) on the line above
+
+        doubl oddsOfSurvival = 7.98;
+        // Doubles are variables with two decimal places
+
+        boolean ansOne  true;
+        // Booleans take true or false values
+
+        char meaningOfLife = '42';
+        //Char holds single character values
+
+        String godPeople = "Deep Thought"
+        //String contains phrases and quotes
+
+        System.out.println("I watching TV. What do you want?");
+        //White space is used to make the code more readable to others
+
+
+
+        System.out.println("There are" + yearsTillDoomsday + " years until Doomsday." );
+
+        System.out.println("Yet we still stand "  oddsOfSurvival + " percent chance of NOT dying.");
+
+
+}
